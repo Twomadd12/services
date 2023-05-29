b6bd022ed26b1fff6d29f0084836a79a5133d5a4
Lesha Egel <twomadd12@gmail.com>
2023-05-29 18:45:50 +0300

    create Arrowbutton

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 let button = document.getElementById('toggleButton')
 let content = document.getElementById('content__other')
+let buttonArrow = document.getElementById('buttonArrow');
 let isContentVisible = false;
+
 
 button.addEventListener('click', function(){
     if (isContentVisible){
     content.style.display = 'none'; 
-        
+    buttonArrow.classList.remove('rotate-180');
         button.textContent = 'Показать все';
         isContentVisible = false;
     }else{
         content.style.display = 'block'
         button.textContent = 'Свернуть'
+        buttonArrow.classList.add('rotate-180');
         isContentVisible = true;
         
     }
 });
+
+
 
 var swiper = new Swiper('.swiper-container', {
     
